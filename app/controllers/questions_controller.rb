1bf5aac8108e381eb45beef5af143598c6d42741
Koki Takahashi <kokita.0317@gmail.com>
2022-08-08 13:16:11 +0900

    completed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @answer =
+      if params[:answer] == 'I am going to work'
+        'Great!'
+      elsif params[:answer].include?('?')
+        'Silly question, get dressed and go to work!'
+      else
+        "I don't care, get dressed and go to work!"
+      end
+  end
+end
